5c07ec74384a25e3b84ce1f76b64c43add67b027
fikri65 <muhammadfikri090905@gmail.com>
2025-09-15 10:07:26 +0700

    Menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, velit.</p>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate corrupti fugiat voluptate? Laboriosam, maxime quisquam?</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
